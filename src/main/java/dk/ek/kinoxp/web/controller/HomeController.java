218c841c47e05a992afa7e32f5dceb3e8cb4740c
djh06c <djdavidjorgen@gmail.com>
2025-10-03 14:17:05 +0200

    Forside, Ret i film - lidt andet

---
+++ src/main/java/dk/ek/kinoxp/web/controller/HomeController.java
@@ -0,0 +1,33 @@
+package dk.ek.kinoxp.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/home";
+    }
+
+    @GetMapping("/billetter")
+    public String tickets() {
+        return "home/billetter";
+    }
+
+    @GetMapping("/mad-drikke")
+    public String food() {
+        return "home/placeholder";
+    }
+
+    @GetMapping("/kalender")
+    public String calendar() {
+        return "home/placeholder";
+    }
+
+    @GetMapping("/vagtplan")
+    public String shifts() {
+        return "home/placeholder";
+    }
+}
