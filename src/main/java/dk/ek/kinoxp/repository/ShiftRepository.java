993f7c288c33d97cee489d97137d2e35db7e9445
Tarik2300 <tana0001@stud.kea.dk>
2025-10-14 18:45:14 +0200

    Feature add shift domain og repository

---
+++ src/main/java/dk/ek/kinoxp/repository/ShiftRepository.java
@@ -0,0 +1,6 @@
+package dk.ek.kinoxp.repository;
+
+import dk.ek.kinoxp.domain.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftRepository extends JpaRepository<Shift, Long> { }
